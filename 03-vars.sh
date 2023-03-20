6250da262fb6951429da3a6e5b51d698062d5272
pavani <pavani.th93@gmail.com>
2023-03-20 11:38:27 +0000

    command and arithmetic substitution

--- 03-vars.sh
+++ 03-vars.sh
@@ -1,2 +1,10 @@
 DATE=2023-03-19
 echo "Hello, Good Morning, Today's Date is ${DATE}" 
+
+# Command substitution
+DATE=$(date)
+echo "Today's date is ${DATE}"
+
+#Arithmetic substitution
+ADD=$((4+5))
+echo "sum of 4+5=${ADD}"
