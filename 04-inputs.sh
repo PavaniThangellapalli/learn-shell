3022a2ca7adbfe0743b1189b30d6b88bb4ea02cc
pavani <pavani.th93@gmail.com>
2023-03-20 13:49:48 +0000

    dynamic arguments

--- 04-inputs.sh
+++ 04-inputs.sh
@@ -1,2 +1,11 @@
-echo $1
-echo $2
+# $0-n, $* / $@, $#
+
+echo First argument = $1 # First argument
+echo Second argument = $2 # Second argument
+
+echo Script name = $0 # Script name
+
+echo $*
+echo All arguments = $@ # All arguments
+
+echo Number of Arguments = $# # Number of arguments
