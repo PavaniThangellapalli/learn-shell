70605d992c3349fe05c359e5d37c264613dfa136
pavani <pavani.th93@gmail.com>
2023-03-21 12:26:55 +0000

    implementing variables

--- 05-functions.sh
+++ 05-functions.sh
@@ -5,8 +5,12 @@ xyz() {
     echo second arg - $2
     echo All args - $*
     echo No of args - $#
+    echo value of a - $a
+    b=200
 }
 
 #main program
 #Call a function
+a=100
 xyz 123 456
+echo value of b - $b
